22017bb4f5b8973e325594387a54fac838e58c27
Anna Delgado Tejedor <s162178@computerome01.cm.cluster>
2019-04-21 17:52:16 +0200

    Final code with testing and error handling implemented. Still need to write help message.

--- scripts/count_reads.sh
+++ scripts/count_reads.sh
@@ -1,35 +1,42 @@
 #!/bin/sh
 
 #Help message? - yes or no? 
+usage="Working on the help message." 
 
 #Read parameters from command line: 
-read_pair = $1
-file = $2
+read_pair=$1
+file=$2
 
 #Check if file exists and proceed if it does: 
-if test -f "$FILE"; then
+if test -f "$file"; then
 
-	case read_pair in:
+	case $read_pair in
 
 		#Paired-end reads:
-		"PE") 
-			reads = wc -l $2
-			total_reads = reads*2
+		"PE")
+			echo $(cat $file | wc -l)/2|bc
+			;;
 
 		#Single-end reads:
 		"SE")
-			total_reads = wc -l $2
+			echo $(cat $file | wc -l)/4|bc
+			;;
 
 		#Display help message:
 		"-h")
 			echo $usage
+			exit 1
+			;;
 
 		#Error message: 
 		*)
 			echo "Please introduce a valid option (PE/SE). For more information, use -h option."
+			exit 1
+			;;
 	esac
 
 else
 	echo "Please introduce a valid file path to your trimmed reads."
 
-return total_reads
+fi
+
