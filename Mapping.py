2875cf510195558492410f54b8cf1918df0c7cb2
ADelgadoT <anna.delgado.tejedor@gmail.com>
2019-04-22 23:57:27 +0200

    Initial draft of parser.

---
+++ Mapping.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+## PARSER for executing all scripts needed for the mapping procedure. 
+
+##Import all required libraries and input data: 
+#Load libraries: 
+import sys
+
+#Load scripts: 
+sys.path.append("./scripts") 
+
