9e6b35ee7ec56ce2f7d5061d0ddb6f61cdc3b6b7
Anna Delgado Tejedor <s162178@risoe-r05-cn178.cm.cluster>
2019-04-23 14:46:45 +0200

    Parser updated.

--- Mapping.py
+++ Mapping.py
@@ -5,7 +5,25 @@
 ##Import all required libraries and input data: 
 #Load libraries: 
 import sys
+import subprocess
 
-#Load scripts: 
-sys.path.append("./scripts") 
+#Update path:
+sys.path.insert(0, "./scripts")
+import Mapping_Results_Processing
+import Prep_Barcode_Generation_BM
 
+##Initialize loop for all samples in input file: 
+sample_data = open(sys.argv[1], 'r')
+
+for line in sample_data:
+	col = line.split()
+
+	##Count total sample reads: 
+	s = subprocess.check_output(["./scripts/count_reads.sh","PE", col[0], col[1]]) 
+	print(int(s))
+	
+	##Prepare kma script and execute it: 
+	kma = Prep_Barcode_Generation_BM.prep_kma(col[0], col[1], col[2], "./scripts/mapping_model.sh")
+
+	execute_kma = subprocess.Popen(kma, shell=True)
+	execute_kma.communicate()
