1306baffdbc898c9994822c7a4610b2f90d6b6a0
Anna Delgado Tejedor <s162178@computerome01.cm.cluster>
2019-04-25 13:26:04 +0200

    Update arguments for the function

--- scripts/Prep_Barcode_Generation_BM.py
+++ scripts/Prep_Barcode_Generation_BM.py
@@ -1,6 +1,6 @@
 ### Prep_Barcode_Generation_BM.py generates all kma scripts to generate their barcodes. They will also output the execution time for benchmarking purposes
 
-def prep_kma(path_forward_reads, path_reverse_reads, sample_name, kma_script_model):
+def prep_kma(path_forward_reads, path_reverse_reads, sample_name, kma_script_model, kma_path, db_path, out_path):
 
     #Extract data from input file and create script: 
     kma_script = open(kma_script_model, 'r')
@@ -11,9 +11,12 @@ def prep_kma(path_forward_reads, path_reverse_reads, sample_name, kma_script_mod
         tmp_line = line.replace('path_R1', path_forward_reads)
         tmp_line2 = tmp_line.replace('path_R2', path_reverse_reads)
         tmp_line3 = tmp_line2.replace('sample_name', sample_name)
+        tmp_line4 = tmp_line3.replace('kma_dir', kma_path)
+        tmp_line5 = tmp_line4.replace('out_dir', out_path)
+        final = tmp_line5.replace('database', db_path)
     
     #Close output file: 
     #out_file.close()
     kma_script.close()
 
-    return tmp_line3
+    return final
