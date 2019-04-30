4dbb36ffdd42e4e02ea154d7e1805ae003a86a89
Anna Delgado Tejedor <s162178@risoe-r04-cn093.cm.cluster>
2019-04-30 12:12:02 +0200

    Updated version.

--- scripts/barcodes_model.sh
+++ scripts/barcodes_model.sh
@@ -1,1 +1,1 @@
-kma_dir -ipe path_R1 path_R2 -i path_R1 -t_db database_backbone -mem_mode -1t1 -ef -t 4 -ck -gapextend 0 -o out_dir/sample_name_Mapping
+kma_dir -ipe path_R1 path_R2 -i path_R1 -t_db database_backbone -mem_mode -1t1 -ef -t 4 -ck -gapextend 0 -o out_dir/sample_name_Barcodes
